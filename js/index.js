497e5181be8a2d4588a2a383cc6bbb39c212d116
Pureruby99 <rpalsson@gmail.com>
2021-04-23 23:11:43 -0600

    asdf

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-const date = new Date();
+const yearElement = document.querySelector(‘#theyear’);
+yearElement.innerText = new Date().getFullYear;
+//const date = new Date();
 
 
-document.querySelector(‘#theyear’).textContent = date.getFullYear;
-document.getElementById(“lastmod”).textContent = document.lastModified;
+//document.querySelector(“#theyear”).textContent = date.getFullYear;
+//document.getElementById(“lastmod”).textContent = document.lastModified;
